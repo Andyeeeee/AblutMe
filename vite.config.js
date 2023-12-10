1797ce9fe02ebd824d7a1fb9da3303dc7799eea9
Andyeeeee <10514f074@gmail.com>
2023-12-10 20:35:01 +0800

    base2

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue({
       template: { transformAssetUrls }
@@ -37,6 +38,5 @@ export default defineConfig({
   },
   server: {
     port: 3000
-  },
-  base: './'
+  }
 })
